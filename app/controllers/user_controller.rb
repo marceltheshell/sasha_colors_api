76ba14b0eed3301c5412383a94022de30b91e14d
marceltheshell <marcel.degas@gmail.com>
2018-05-28 22:52:54 -0700

    writes a create method in user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,17 @@
 class UserController < ApplicationController
-  
+
+
+  def create 
+    @user = User.create!(
+      first_name: params[first_name], 
+      last_name: params[last_name], 
+      phone_number: params[phone_number]
+    )
+    render json: @user.to_json
+  end
+  private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number)
+  end
 end
